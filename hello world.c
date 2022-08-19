9f59d08ce0784ebaae626356968184a6d3be095e
hoang046 <111508873+hoang046@users.noreply.github.com>
2022-08-19 14:04:24 +0700

    first commit
    test commit content

---
+++ hello world.c
@@ -0,0 +1,15 @@
+#include <stdio.h
+#include <stdio.h>
+
+/* run this program using the cansole pauser or add your own getc, system 
+
+int main(int argc, char *argv []) { 
+    int num1 ;
+    int num2 ;
+    printF("num1 = ") ;
+    scanF("%d",&num1) ;
+    printF("num2 = ") ;
+    santf("%d",&num2) ;
+    //printf("%d+%d=%d", num1,num2,num1+num2);
+    return 0 ;
+}
